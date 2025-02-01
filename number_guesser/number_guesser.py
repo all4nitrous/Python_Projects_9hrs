ea9a3763819f03826e9945df7ee455beec6e16a6
all4nitrous <all4nitrous@gmail.com>
2025-02-01 12:01:59 -0500

    number_guesser finished

--- number_guesser/number_guesser.py
+++ number_guesser/number_guesser.py
@@ -29,10 +29,9 @@ while True:
     if user_guess == random_number:
         print("You got it!")
         break
-    else:
-        if user_guess > random_number:
+    elif user_guess > random_number:
             print("You were above the number!")
-        else:
+    else:
             print("You were below the number!")
 
 print("You got it in", guesses, "guesses")
